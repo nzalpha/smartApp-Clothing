46f8d110b2ca32e877871c14adc55253a25a650e
nzalpha <148647509+nzalpha@users.noreply.github.com>
2025-02-10 20:15:20 -0800

    config end points

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://35.224.78.158:30001/api'
-const usersServiceDevHost = 'http://35.224.78.158:30722/api'
+const produtsServiceDevHost = 'http://104.197.6.198:5132/api'
+const usersServiceDevHost = 'http://104.197.6.198:5232/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
